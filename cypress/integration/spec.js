05185026137531c8a2e81f5768b6e1cfe9264182
george <longlandg@hotmail.com>
2019-04-29 11:06:56 +0100

    first 4 cypress tests complete

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,15 +4,24 @@ describe("My First Test", function() {
   });
 });
 
-describe("visiting HomePageView page works", () => {
+describe("HomPageView", () => {
   it("Visits NC-NEWS", () => {
     cy.visit("");
   });
-});
+  it("finds the content LOG IN button, clicks, then goes to url with /signin ", function() {
+    cy.visit("http://localhost:3002/signin");
 
+    cy.contains("LOG IN").click();
+    cy.url().should("include", "/signin");
+  });
+  it("finds the 'comment count ascending' within the sort by drop down", function() {
+    cy.visit("http://localhost:3002");
+    cy.get(".sortBySelector").select("comment count ascending");
+  });
+});
 describe("visiting SignInPageView page works", () => {
   it("Visits NC-NEWS", () => {
-    cy.visit("/http://localhost:3001/signin");
+    cy.visit("/http://localhost:3002/signin");
   });
 });
 
@@ -24,20 +33,12 @@ describe("visiting SignInPageView page works", () => {
 //     cy.url().should("include", "/users");
 //   });
 // });
-describe("/home", function() {
-  it('finds the content "type"', function() {
-    cy.visit("http://localhost:3001/signin");
 
-    cy.contains("LOG IN").click();
-    cy.url().should("include", "/signin");
-  });
-});
+// describe("/home", function() {
+//   it('finds the content "type"', function() {
+//     cy.visit("http://localhost:3002/topics/createtopic");
 
-describe("/home", function() {
-  it('finds the content "type"', function() {
-    cy.visit("http://localhost:3001/topics/createtopic");
-
-    cy.contains("LOG OUT").click();
-    cy.url().should("include", "/");
-  });
-});
+//     cy.contains("LOG OUT").click();
+//     cy.url().should("include", "/");
+//   });
+// });
